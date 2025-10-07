16deafb4dd57682eb36f2e14c0dfc293c735842b
raffinauvall <raffinauvaltaqy@gmail.com>
2025-10-08 00:03:33 +0700

    feat: add register function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request) {
+        $request->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|unique:users',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return redirect()->route('login')->with('success', 'Registrasi berhasil! Silahkan login.');
+    }
+
+    
 }
