0cfeb5d386d54d84f82a903d1f7553b24ab5854b
raffinauvall <raffinauvaltaqy@gmail.com>
2025-10-07 23:23:14 +0700

    first commit

---
+++ database/migrations/2025_10_07_160616_create_table_transactions.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('description');
+            $table->foreignId('categories_id')->constrained('categories')->onDelete('cascade');
+            $table->decimal('amount', 15, 2);
+            $table->foreignId('users_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('table_transactions');
+    }
+};
