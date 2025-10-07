a75410c0cc4a8b73b61b507b94e0959ff3f2214c
raffinauvall <raffinauvaltaqy@gmail.com>
2025-10-08 00:17:16 +0700

    feat: add route for register and login (page, function)

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Login
+Route::get('/login', [UserController::class, 'showLogin'])->name('register');
+Route::post('/login', [UserController::class, 'login']);
+
+// Register
+Route::get('/register', [UserController::class, 'showRegister'])->name('login');
+Route::post('/register', [UserController::class, 'register']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
