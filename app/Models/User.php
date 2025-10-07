a7d89ed0e092e35548c9f9b3897810154a3169b0
Japraannn <nougy009@gmail.com>
2025-10-07 23:51:31 +0700

    feat: add users model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Model
+class User extends Authenticatable
 {
-    //
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    // Relasi ke transaksi
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    // Relasi ke kategori
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
 }
