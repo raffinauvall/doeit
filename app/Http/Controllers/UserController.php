bfb3ea83b0f4bbdae32bf059c318f142afbcae27
raffinauvall <raffinauvaltaqy@gmail.com>
2025-10-08 00:08:15 +0700

    feat: add login and logout function to UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,5 +22,28 @@ class UserController extends Controller
         return redirect()->route('login')->with('success', 'Registrasi berhasil! Silahkan login.');
     }
 
+    public function login(Request $request) {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('dashboard');
+        } 
+
+        return back()->withErrors([
+            'email' => 'Email atau password salah'
+        ])->onlyInput('email');
+    }
+
+    public function logout(Request $request) {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
     
 }
