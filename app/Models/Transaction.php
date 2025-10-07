c0f64c2477f14def3c2c33f8b01df7fdc37518ad
raffinauvall <raffinauvaltaqy@gmail.com>
2025-10-07 23:53:29 +0700

    feat: add transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'tanggal',
+        'keterangan',
+        'jumlah',
+        'kategori_id',
+        'user_id',
+    ];
+
+    // Relasi ke user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relasi ke kategori
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'kategori_id');
+    }
 }
