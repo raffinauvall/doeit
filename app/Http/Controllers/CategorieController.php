de5809c47dc23d047c2a05712b02e63b91198c4c
raffinauvall <raffinauvaltaqy@gmail.com>
2025-10-08 16:28:10 +0700

    feat: add crud

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -3,8 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CategorieController extends Controller
 {
+    public function index() {
+        $categories = Categorie::where('users_id', Auth::id())->get();
+        return view('categories.index', compact('categories'));
+    }
 
+    public function create() {
+        return view('categories.create');
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'name' => 'required|string|max:100',
+            'tipe' => 'required|in:pemasukan,pengeluaran',
+        ]);
+
+        Categories::create([
+            'name' => $request->name,
+            'tipe' => $request->tipe,
+            'users_id' => Auth::id(),
+        ]);
+
+        return redirect()->route('categories.index')->with('success', 'Kategori berhasil ditambahkan');
+    }
+
+    public function edit(Request $request, $id) {
+        $request->validate([
+            'name' => 'string|max:100',
+            'tipe' => 'in:pemasukan, pengeluaran',
+        ]);
+
+        $categories = Categorie::findOrFail($id);
+        $categories->update([
+            'name' => $request->name,
+            'tipe' => $request->tipe,
+        ]);
+
+        return redirect()->route('categories.index')->with('success', 'Kategori berhasil diupdate');
+    }
+
+    public function destroy(Request $request, $id) {
+        $categories = Categorie::findOrFail($id);
+        $categories->delete();
+        
+        return redirect()->route('categories.index')->with('success', 'Kategori berhasil dihapus');
+    }
 }
