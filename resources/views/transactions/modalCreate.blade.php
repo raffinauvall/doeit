8120c02557b00f0aa1653d6b9bf0b409148333d5
raffinauvall <raffinauvaltaqy@gmail.com>
2025-10-13 01:50:14 +0700

    feat: add component (modalCreate)

---
+++ resources/views/transactions/modalCreate.blade.php
@@ -0,0 +1,45 @@
+<div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="createModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header bg-success text-white">
+        <h5 class="modal-title" id="createModalLabel">Create Transaction</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+
+      <form action="{{ route('transactions.store') }}" method="POST">
+        @csrf
+        <input type="hidden" name="user_id" value="{{ Auth::id() }}">
+        <div class="modal-body">
+          <div class="mb-3">
+            <label class="form-label">Description</label>
+            <input type="text" name="description" class="form-control" required>
+          </div>
+          <div class="mb-3">
+          <label class="form-label">Date</label>
+            <input type="date" name="date" class="form-control" required>
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Category</label>
+            <input type="text" name="category" class="form-control"  required>
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Amount</label>
+            <input type="number" name="amount" class="form-control" required>
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Type</label>
+            <select name="type" class="form-select" required>
+              <option value="income">Income</option>
+              <option value="expense">Expense</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+          <button type="submit" class="btn btn-success text-white">Simpan</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
